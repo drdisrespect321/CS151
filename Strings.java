e0278665319c95c8f38836d54eff83d9481f8bd4
drdisrespect321 <47164657+drdisrespect321@users.noreply.github.com>
2019-01-31 14:59:36 -0800

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -3,13 +3,13 @@ public class Strings {
     public static String uniqueLetters(String str){
         String unique = "";
         for(int i = 0; i < str.length(); ++i){
-            int count = 0;
+            int same = 0;
             for(int j = 0; j < str.length(); ++j){
-                if(str.charAt(i) == str.charAt(j)){
-                    ++count;
+                if(str.substring(i,i+1).equals(str.substring(j,j+1))){
+                    same++;
                 }
             }
-            if(count == 1){
+            if(same==1){
                 unique += String.valueOf(str.charAt(i));
             }
         }
